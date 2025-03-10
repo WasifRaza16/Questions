abd6dda9469d4598bf6cdcb934026b98430afbe5
WasifRaza16 <144607296+WasifRaza16@users.noreply.github.com>
2025-03-10 14:31:02 +0530

    Add files via upload

---
+++ practice/2darray.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<"Enter for row : "<<i+1<<" column : "<<j+1<<endl;
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Array :"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Sum of rows :"<<endl;
+    for(int i=0;i<3;i++){
+        int sum=0;
+        for(int j=0;j<3;j++){
+            sum=sum+arr[i][j];
+        }
+        cout<<i+1<<" row : "<<sum<<endl;
+    }
+    cout<<"Sum of columns :"<<endl;
+    for(int i=0;i<3;i++){
+        int sum=0;
+        for(int j=0;j<3;j++){
+            sum=sum+arr[j][i];
+        }
+        cout<<i+1<<" column : "<<sum<<endl;
+    }
+}
+/*
+background-image: url(image.jpg);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+         	height: 100vh;
+	        width: 100%;
+	        position: fixed; 
+	        top: 0;
+	        left: 0;
+*/
